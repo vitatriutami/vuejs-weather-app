e1b7cddd7c36502a8cc7559b35e461880bbc370f
Vita <vitatriutami@gmail.com>
2024-11-21 22:50:47 +0700

    add: fix display place button

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,17 +1,26 @@
 <script setup>
-  import { reactive } from 'vue'
+import { reactive } from 'vue'
 
-  const searchTerm = reactive({
-    query: '',
-    timeout: null
-  })
+const searchTerm = reactive({
+  query: '',
+  timeout: null,
+  results: null,
+})
 
-  const handleSearch = () => {
-    clearTimeout(searchTerm.timeout)
-    searchTerm.timeout = setTimeout(async () => {
-      const res = await fetch(`http://api.weatherapi.com/v1/search.json?key=ea967b703edb4914b62153351242111&q=${searchTerm.query}`)
-    }, 500);
-  }
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout)
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.query !== '') {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/search.json?key=ea967b703edb4914b62153351242111&q=${searchTerm.query}`,
+      )
+
+      const data = await res.json()
+      searchTerm.results = data
+      console.log(searchTerm.results)
+    }
+  }, 500)
+}
 </script>
 
 <template>
@@ -25,14 +34,16 @@
           placeholder="Search for a place"
           class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
           v-model="searchTerm.query"
-          @input = "handleSearch"
+          @input="handleSearch"
         />
       </div>
     </form>
     <!-- search suggestions -->
     <div class="bg-white my-2 rounded-lg shadow-lg">
-      <div>
-        <button class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"></button>
+      <div v-for="place in searchTerm.results" :key="place.id">
+        <button class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left">
+          {{ place.name }}, {{ place.region }}, {{ place.country }}
+        </button>
       </div>
     </div>
   </div>
