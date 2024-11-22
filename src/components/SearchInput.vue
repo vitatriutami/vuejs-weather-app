b09bc06f5724f205de22a47c1bc50e7875989c68
Vita <vitatriutami@gmail.com>
2024-11-22 09:31:58 +0700

    add: fixed cors

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -14,7 +14,7 @@ const handleSearch = () => {
   searchTerm.timeout = setTimeout(async () => {
     if (searchTerm.query !== '') {
       const res = await fetch(
-        `http://api.weatherapi.com/v1/search.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=${searchTerm.query}`
+        `https://api.weatherapi.com/v1/search.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=${searchTerm.query}`
       )
 
       const data = await res.json()
@@ -27,7 +27,7 @@ const handleSearch = () => {
 
 const getWeather = async (id) => {
   const res = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=id:${id}&days=3&aqi=no&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=id:${id}&days=3&aqi=no&alerts=no`
   )
   const data = await res.json()
 
